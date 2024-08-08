293121d76afcd67670b2a02edcd707d37cb669f9
Paiman-Rasoli <paimanrasoli789@gmail.com>
2024-08-08 16:48:48 +0430

    fix issue with es module

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   format: ["esm", "cjs"], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
-  splitting: false,
   sourcemap: true,
   clean: true,
+  outDir: "dist",
 });
