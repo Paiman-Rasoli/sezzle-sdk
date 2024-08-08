e81abe2cfe7edd684e7f1685578efe67223cee73
Paiman-Rasoli <paimanrasoli789@gmail.com>
2024-08-08 17:04:41 +0430

    make package for avaliable for node enviroment

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
+  entry: ["./src/index.ts"],
   format: ["esm", "cjs"], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
   sourcemap: true,
